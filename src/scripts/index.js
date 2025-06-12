4c5db7e77899ea03604b6135121252ba0b514898
AceDev <Alexgustavo13@gmail.com>
2025-06-12 08:56:36 -0300

    Initializing registration screen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,16 @@
 const body = document.querySelector('body')
+const firstLoginButton = document.querySelector('.firstLoginButton')
+const loginModal = document.querySelector('.loginModal')
+const buttonReturn = document.querySelector('.returnHome')
+
+firstLoginButton.addEventListener('click', () => {
+    loginModal.classList.remove('hidden')
+});
+
+buttonReturn.addEventListener('click', ()=> {
+    loginModal.classList.add('hidden')
+})
+
 
 
 
